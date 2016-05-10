5d3aa59304d181fbba660e7298abfc894d14cc3d
Mireia <mireia@mireiasangalo.com>
2016-05-10 16:54:02 +0100

    more code

--- frontend/src/js/components/Aims/index.js
+++ frontend/src/js/components/Aims/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {Tabs, Tab} from 'react-materialize'
 
-export default class Aims extends Component {
+export default class Aims extends React.Component {
+  render() {
     return (
       <Tabs className='tab-demo z-depth-1'>
           <Tab title='Current amis'>Test 1</Tab>
