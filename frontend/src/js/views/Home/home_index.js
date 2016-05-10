731f6a3e1a3c3c0634598cf06c58baae4f956732
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-05-10 22:14:28 +0100

    adding mecards ttinder

--- frontend/src/js/views/Home/home_index.js
+++ frontend/src/js/views/Home/home_index.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Row, Col } from 'react-materialize'
 import Cards from '../../components/Cards/'
+
 import HomePage from '../../components/HomePage/'
 
 export default class Home extends React.Component {
   render () {
     return (
-      <Cards/>
+      <HomePage />
     )
   }
 }
