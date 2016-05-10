953764e858e1d29622937ca849484f59d3083b1b
Katherine Bowler <katherine.bowler@gmail.com>
2016-05-10 14:32:07 +0100

    Add in materialize button

--- frontend/src/js/components/HomePage/index.js
+++ frontend/src/js/components/HomePage/index.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
-
-import Button from './Button.js'
+import { Button } from 'react-materialize'
 
 export default class HomePage extends Component {
   render () {
     return (
       <div>
         <h2>I am a Survivor</h2>
-        <Link to={'/contact'}><Button buttonText='Sign Up'/></Link>
+        <Link to={'/contact'}><Button waves='light'>Sign Up</Button></Link>
         <h2>I am a Supporter</h2>
-        <Link to={'/about'}><Button buttonText='Sign Up'/></Link>
+        <Link to={'/about'}><Button waves='light'>Sign Up</Button></Link>
       </div>
     )
   }
