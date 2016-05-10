80d5bd158312ac49db84786a7ea69939be3f2575
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-05-10 15:37:56 +0100

    removing bootstrap

--- frontend/src/js/views/Home/home_index.js
+++ frontend/src/js/views/Home/home_index.js
@@ -1,21 +1,14 @@
 import React from 'react'
-import { Grid, Row, Col } from 'react-bootstrap'
-import { connect } from 'react-redux'
-import { getUserDetails } from '../../actions/actions_index.js'
-
+import { Row, Col } from 'react-materialize'
+import Cards from '../../components/Cards/'
 export default class Home extends React.Component {
   render () {
     return (
-      <Grid className='home'>
-        <Row>
-          <Col xs={12}>
-            <div className='image-container'>
-              <img src='img/rhino.png' />
-            </div>
-            <p>Your app goes here...</p>
-          </Col>
-        </Row>
-      </Grid>
+      <Row>
+        <Col s={10} m={10} l={10} offset='s2 m2 l2'>
+          'hai'
+        </Col>
+      </Row>
     )
   }
 }
